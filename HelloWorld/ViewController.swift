246aebeac8f46b7b61b2d27b2777acd7c4b3c9b4
Yasyf Mohamedali <yasyfm@gmail.com>
2015-02-14 16:28:19 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Yasyf Mohamedali on 2015-02-14.
+//  Copyright (c) 2015 Yasyf Mohamedali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
